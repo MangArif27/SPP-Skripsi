988530eea70c64b9082d547014a88aa58502e119
muarif27 <muarif130912@gmail.com>
2024-03-24 23:09:45 +0700

    Membuat Data Sekolah

--- app/Http/Controllers/InsertController.php
+++ app/Http/Controllers/InsertController.php
@@ -6,6 +6,10 @@ use App\Models\ModelSiswa;
 use App\Imports\ImportSiswa;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 
 class InsertController extends Controller
 {
@@ -28,5 +32,32 @@ class InsertController extends Controller
         //Session::flash('sukses', 'Data Siswa Berhasil Diimport!');
         // alihkan halaman kembali
         return redirect('/Data-Siswa');
+    }
+    public function InsertSekolah(Request $request)
+    {
+        if ($request->Tahun_Ajaran == "InputBaru") {
+            $TahunAjaran = $request->TahunAjaranBaru;
+            DB::table('tahun_ajaran')->insert([
+                'tahun_ajaran' => $TahunAjaran,
+            ]);
+        } else {
+            $TahunAjaran = $request->TahunAjaran;
+        }
+        DB::table('pengaturan')->where('id', $request->NPSN)->update([
+            'sekolah' => $request->Nama_Sekolah,
+            'alamat' => $request->Alamat,
+            'telpon' => $request->Telp,
+            'faximile' => $request->Fax,
+            'email' => $request->Email,
+            'website' => $request->Website,
+            'nama_kepsek' => $request->Nama_Kepsek,
+            'nama_bendahara' => $request->Nama_Bendahara,
+            'jumlah_kejuruan' => $request->Jumlah_Kejuruan,
+            'jumlah_gtk' => $request->Jumlah_GTK,
+            'jumlah_kelas' => $request->Jumlah_Kelas,
+            'tahun_ajaran' => $TahunAjaran,
+            'semester' => $request->Semester,
+        ]);
+        return redirect('/Pengaturan');
     }
 }
--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
@@ -25,5 +29,11 @@ class PageController extends Controller
     public function Tagihan()
     {
         return view('Page._DataTagihan');
+    }
+    public function Pengaturan()
+    {
+        $Sekolah = DB::table('pengaturan')->get();
+        $Tahun = DB::table('tahun_ajaran')->get();
+        return view('Page._Pengaturan', ['Sekolah' => $Sekolah, 'Tahun' => $Tahun]);
     }
 }
