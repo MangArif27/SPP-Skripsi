a6b0671b20242f08307d1cf52b6b5073a444b137
muarif27 <muarif130912@gmail.com>
2024-04-17 13:27:17 +0700

    Update Menu Sidebar

--- resources/views/Partials/_Sidebar.blade.php
+++ resources/views/Partials/_Sidebar.blade.php
@@ -39,7 +39,7 @@
                     </li>
                     <li>
                         <a href="Data-Tagihan">
-                            <span class="pcoded-mtext">Jenis Tagihan</span>
+                            <span class="pcoded-mtext">Data Tagihan</span>
                         </a>
                     </li>
                 </ul>
