167faeb5bfbedc3ef3f9b2bdf2374e38db75b91f
MangArif27 <muarif130912@gmail.com>
2024-06-15 17:26:54 +0700

    Membuat Statistik Radial

--- resources/views/Page/_HomePage.blade.php
+++ resources/views/Page/_HomePage.blade.php
@@ -52,24 +52,110 @@
                                         $SudahLunas = DB::table('pembayaran_spp')->where('tahun_ajaran', $Pengaturan->tahun_ajaran)->where('semester', $Pengaturan->semester)->where('keterangan', 'Sudah Lunas')->count();
                                         $BelumLunas = DB::table('pembayaran_spp')->where('tahun_ajaran', $Pengaturan->tahun_ajaran)->where('semester', $Pengaturan->semester)->where('keterangan', 'Belum Lunas')->count();
                                         if ($SudahLunas == 0) {
-                                            $PersentaseLunas =  $SudahLunas * 100;
+                                            $Persentase =  $SudahLunas * 100;
                                         } else {
-                                            $PersentaseLunas =  $SudahLunas / $JumlahAll * 100;
+                                            $Persentase =  $SudahLunas / $JumlahAll * 100;
+                                        }
+                                        if ($Persentase <= 6) {
+                                            $PersentaseLunas = 5;
+                                        } elseif ($Persentase <= 11) {
+                                            $PersentaseLunas = 10;
+                                        } elseif ($Persentase <= 16) {
+                                            $PersentaseLunas = 15;
+                                        } elseif ($Persentase <= 21) {
+                                            $PersentaseLunas = 20;
+                                        } elseif ($Persentase <= 26) {
+                                            $PersentaseLunas = 25;
+                                        } elseif ($Persentase <= 31) {
+                                            $PersentaseLunas = 30;
+                                        } elseif ($Persentase <= 36) {
+                                            $PersentaseLunas = 35;
+                                        } elseif ($Persentase <= 41) {
+                                            $PersentaseLunas = 40;
+                                        } elseif ($Persentase <= 46) {
+                                            $PersentaseLunas = 45;
+                                        } elseif ($Persentase <= 51) {
+                                            $PersentaseLunas = 50;
+                                        } elseif ($Persentase <= 56) {
+                                            $PersentaseLunas = 55;
+                                        } elseif ($Persentase <= 61) {
+                                            $PersentaseLunas = 60;
+                                        } elseif ($Persentase <= 66) {
+                                            $PersentaseLunas = 65;
+                                        } elseif ($Persentase <= 71) {
+                                            $PersentaseLunas = 70;
+                                        } elseif ($Persentase <= 76) {
+                                            $PersentaseLunas = 75;
+                                        } elseif ($Persentase <= 81) {
+                                            $PersentaseLunas = 80;
+                                        } elseif ($Persentase <= 86) {
+                                            $PersentaseLunas = 85;
+                                        } elseif ($Persentase <= 91) {
+                                            $PersentaseLunas = 90;
+                                        } elseif ($Persentase <= 96) {
+                                            $PersentaseLunas = 95;
+                                        } elseif ($Persentase <= 100) {
+                                            $PersentaseLunas = 100;
+                                        } else {
+                                            $PersentaseLunas = 0;
                                         }
                                         if ($BelumLunas == 0) {
-                                            $PersentaseBelum = $BelumLunas * 100;
+                                            $PersentaseX = $BelumLunas * 100;
                                         } else {
-                                            $PersentaseBelum = $BelumLunas / $JumlahAll * 100;
+                                            $PersentaseX = $BelumLunas / $JumlahAll * 100;
+                                            if ($PersentaseX <= 6) {
+                                                $PersentaseBelum = 5;
+                                            } elseif ($PersentaseX <= 11) {
+                                                $PersentaseBelum = 10;
+                                            } elseif ($PersentaseX <= 16) {
+                                                $PersentaseBelum = 15;
+                                            } elseif ($PersentaseX <= 21) {
+                                                $PersentaseBelum = 20;
+                                            } elseif ($PersentaseX <= 26) {
+                                                $PersentaseBelum = 25;
+                                            } elseif ($PersentaseX <= 31) {
+                                                $PersentaseBelum = 30;
+                                            } elseif ($PersentaseX <= 36) {
+                                                $PersentaseBelum = 30;
+                                            } elseif ($PersentaseX <= 41) {
+                                                $PersentaseBelum = 35;
+                                            } elseif ($PersentaseX <= 46) {
+                                                $PersentaseBelum = 40;
+                                            } elseif ($PersentaseX <= 51) {
+                                                $PersentaseBelum = 45;
+                                            } elseif ($PersentaseX <= 56) {
+                                                $PersentaseBelum = 55;
+                                            } elseif ($PersentaseX <= 61) {
+                                                $PersentaseBelum = 55;
+                                            } elseif ($PersentaseX <= 66) {
+                                                $PersentaseBelum = 60;
+                                            } elseif ($PersentaseX <= 71) {
+                                                $PersentaseBelum = 65;
+                                            } elseif ($PersentaseX <= 76) {
+                                                $PersentaseBelum = 70;
+                                            } elseif ($PersentaseX <= 81) {
+                                                $PersentaseBelum = 75;
+                                            } elseif ($PersentaseX <= 86) {
+                                                $PersentaseBelum = 80;
+                                            } elseif ($PersentaseX <= 91) {
+                                                $PersentaseBelum = 85;
+                                            } elseif ($PersentaseX <= 96) {
+                                                $PersentaseBelum = 90;
+                                            } elseif ($PersentaseX <= 100) {
+                                                $PersentaseBelum = 95;
+                                            } else {
+                                                $PersentaseBelum = 0;
+                                            }
                                         }
                                         ?>
                                         <div class="col-6">
-                                            <div data-label="{{round($PersentaseLunas)}}%" class="radial-bar radial-bar-{{round($PersentaseLunas)}} radial-bar-lg radial-bar-success"></div>
+                                            <div data-label="{{round($Persentase)}}%" class="radial-bar radial-bar-{{$PersentaseLunas}} radial-bar-lg radial-bar-success"></div>
                                             <h6 class="text-muted">Selesai</h6>
                                             <p class="text-muted">{{$SudahLunas}} Berkas</p>
                                             <button class="btn btn-success btn-round btn-sm">Sudah Lunas</button>
                                         </div>
                                         <div class="col-6">
-                                            <div data-label="{{round($PersentaseBelum)}}%"" class=" radial-bar radial-bar-{{round($PersentaseBelum)}} radial-bar-lg radial-bar-danger"></div>
+                                            <div data-label="{{round($PersentaseX)}}%"" class=" radial-bar radial-bar-{{round($PersentaseBelum)}} radial-bar-lg radial-bar-danger"></div>
                                             <h6 class="text-muted">Proses</h6>
                                             <p class="text-muted">{{$BelumLunas}} Berkas</p>
                                             <button class="btn btn-danger btn-round btn-sm">Belum Lunas</button>
